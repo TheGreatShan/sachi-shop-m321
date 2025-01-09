9f691552e13b12698a7143f8920e94162fa06712
Christopher Scheel <christopherscheel99@gmail.com>
2025-01-09 09:59:41 +0100

    listener consumer setup

--- services/logging-service/utils/consumer.go
+++ services/logging-service/utils/consumer.go
@@ -1,1 +1,40 @@
 package utils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func ConsumeListener() {
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.SubscribeTopics([]string{"logs"}, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	run := true
+
+	for run {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			SaveLog(string(msg.Value))
+		} else if !err.(kafka.Error).IsTimeout() {
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	c.Close()
+}
