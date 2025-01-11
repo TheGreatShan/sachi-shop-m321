a15c6f1d4b2381141571bb7234cb0d0ae9831016
chris911e <christopherscheel99@gmail.com>
2025-01-11 21:37:25 +0100

    /profile page

---
+++ sachi-shop-web/src/Profile.tsx
@@ -0,0 +1,46 @@
+export default function Profile() {
+    return(
+        <>
+            <div className='pt-20'>
+                <div className="pt-20 w-full flex justify-center">
+                    <div className="bg-gray-100 w-1/2 p-5 rounded-lg shadow-lg">
+                        <div className="grid grid-cols-5 gap-5">
+                            <div className="col-span-1">
+                                <img src="/person.svg" alt="person" height={200} width={200} />
+                            </div>
+                            <div className="col-span-1">
+                                <p className="font-bold text-2xl">
+                                    Profile
+                                </p>
+                                <div className="grid grid-cols-2 gap-1">
+                                    <div>
+                                        <p>
+                                            First Name:
+                                        </p>
+                                        <p>
+                                            Last Name:
+                                        </p>
+                                        <p>
+                                            Role:
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p>
+                                            John
+                                        </p>
+                                        <p>
+                                            Doe
+                                        </p>
+                                        <p className="text-red-500">
+                                            Admin
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
