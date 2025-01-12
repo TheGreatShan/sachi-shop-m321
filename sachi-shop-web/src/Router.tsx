761481836ce554275d127229b4e51a7d04dd7bdb
chris911e <christopherscheel99@gmail.com>
2025-01-12 12:29:26 +0100

    router for /dashboard

--- sachi-shop-web/src/Router.tsx
+++ sachi-shop-web/src/Router.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import Profile from "./Profile";
 import Cart from "./Cart";
 import Navbar from "./components/Navbar";
+import Dashboard from "./Dashboard";
 
 
 
@@ -14,6 +15,7 @@ function Router() {
               <Route index element={<App/>} />
               <Route path="profile" element={<Profile />} />
               <Route path="cart" element={<Cart />} />
+              <Route path="dashboard" element={<Dashboard />} />
             </Route>
           </Routes>
       </BrowserRouter>
