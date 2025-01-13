0572338e11d33e3b26062305fc64acfd6dc605c5
Christopher Scheel <christopherscheel99@gmail.com>
2025-01-13 10:34:43 +0100

    changed urls

--- sachi-shop-web/src/api/log.ts
+++ sachi-shop-web/src/api/log.ts
@@ -9,7 +9,8 @@ interface Log {
 
 export const getLogs = async () => {
     try {
-        const apiUrl = import.meta.env.VITE_DB_API_URL + '/api/logs';
+        const apiUrl = import.meta.env.VITE_EUREKA_API_URL + '/logging-service/api/logs';
+        console.log(apiUrl)
         const response = await axios.get(apiUrl);
         return response.data
     } catch (error) {
@@ -19,7 +20,7 @@ export const getLogs = async () => {
 
 export const produceLog = async (log: Log) => {
     try {
-        const apiUrl = import.meta.env.VITE_KAFKA_API_URL + '/produce?topic=logs'
+        const apiUrl = import.meta.env.VITE_EUREKA_API_URL + '/event-hub-service/produce?topic=logs'
         console.log(apiUrl)
         const response = await axios.post(apiUrl, log)
         return response.data
