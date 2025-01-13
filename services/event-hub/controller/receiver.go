5e30b5ec696536bff455828f8637e56c4eeba375
Christopher Scheel <christopherscheel99@gmail.com>
2025-01-13 10:37:12 +0100

    moved receiver

---
+++ services/event-hub/controller/receiver.go
@@ -0,0 +1,33 @@
+package controller
+
+import(
+	"fmt"
+	"encoding/json"
+	"net/http"
+
+    "event-hub/utils"
+)
+
+func Receive(w http.ResponseWriter, r *http.Request) {
+    var message map[string]interface{}
+    err := json.NewDecoder(r.Body).Decode(&message)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    topic := r.URL.Query().Get("topic")
+    if topic == "" {
+        w.WriteHeader(http.StatusBadRequest)
+        response := map[string]interface{}{"message": "Missing 'topic' query parameter"}
+        json.NewEncoder(w).Encode(response)
+        return
+    }
+
+    messageString := fmt.Sprintf("%v", message)
+
+    utils.ProduceEvent(messageString, topic)
+
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(message)
+}
