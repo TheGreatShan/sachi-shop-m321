121421bf0fd035ad00ceeccc774820825d6151e9
Shansai Muraleetharan <61542805+TheGreatShan@users.noreply.github.com>
2025-01-14 20:04:42 +0100

    Update SqlConnection.cs

--- services/stock-service/Utils/SqlConnection.cs
+++ services/stock-service/Utils/SqlConnection.cs
@@ -7,7 +7,6 @@ public static class SqlConnection
 {
     public static IDbConnection GetConnection()
     {
-        // TODO - Replace with appsettings.json
         return new MySqlConnection("Server=localhost,3306;Database=inventory;Uid=root;Pwd=humanities@best-password.ByFar2025;");
     }
 }
